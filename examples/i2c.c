39bf412e532bec07bdb337a357b4c6fb80c29a4a
Pieter Noordhuis <pcnoordhuis@gmail.com>
2014-07-12 16:12:59 -0700

    Use millisecond counter in I2C example
    The example now uses the time it takes to perform a measurement and
    write the result to serial to space measurements an even 100ms apart.

--- examples/i2c.c
+++ examples/i2c.c
@@ -7,6 +7,23 @@
 
 #define HMC5883L_ADDRESS (0x1e)
 
+void hmc5883l_measure(FILE *uart) {
+  uint8_t b3[1] = { 0x3 };
+  uint8_t data[6];
+  int16_t axis[3];
+
+  // Read measurement
+  i2c_write(HMC5883L_ADDRESS, b3, 1);
+  i2c_read(HMC5883L_ADDRESS, data, 6);
+
+  // Post-process measurement
+  axis[0 /* X */] = (data[0] << 8) | data[1];
+  axis[2 /* Z */] = (data[2] << 8) | data[3];
+  axis[1 /* Y */] = (data[4] << 8) | data[5];
+
+  fprintf(uart, "X: %5d, Y: %5d, Z: %5d\n", axis[0], axis[1], axis[2]);
+}
+
 void hmc5883l_task(void *unused) {
   uint8_t b0[2] = { 0x0, _BV(4) };
   uint8_t b1[2] = { 0x1, _BV(6) };
@@ -19,23 +36,16 @@ void hmc5883l_task(void *unused) {
   i2c_write(HMC5883L_ADDRESS, b2, 2);
 
   while (1) {
-    uint8_t b3[1] = { 0x3 };
-    uint8_t data[6];
-    int16_t axis[3];
+    uint8_t t1, t2, ms;
 
-    // Wait for measurement
-    task_sleep(100);
+    t1 = task_ms();
+    hmc5883l_measure(&uart);
+    t2 = task_ms();
 
-    // Read measurement
-    i2c_write(HMC5883L_ADDRESS, b3, 1);
-    i2c_read(HMC5883L_ADDRESS, data, 6);
-
-    // Post-process measurement
-    axis[0 /* X */] = (data[0] << 8) | data[1];
-    axis[2 /* Z */] = (data[2] << 8) | data[3];
-    axis[1 /* Y */] = (data[4] << 8) | data[5];
-
-    fprintf(&uart, "X: %5d, Y: %5d, Z: %5d\n", axis[0], axis[1], axis[2]);
+    // This subtraction underflows if t1 > t2, but yields the right result.
+    // Use measured time to space calls to "hmc5883l_measure" 100ms apart.
+    ms = t2 - t1;
+    task_sleep(100 - ms);
   }
 }
 
